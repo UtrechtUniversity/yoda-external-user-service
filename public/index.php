e894d1e7370757dc6728e2bea40dcb2073a06e1f
Chris Smeele <cj.smeele@xs4all.nl>
2018-09-03 14:17:15 +0200

    Fixed default paths

--- public/index.php
+++ public/index.php
@@ -4,18 +4,18 @@
 $APP_ROOT =
     isset($_SERVER['YODA_APP_ROOT'])
         ? $_SERVER['YODA_APP_ROOT']
-        : '../../extuser';
+        : '../../yoda-external-user-service';
 
 if (chdir($APP_ROOT) === FALSE) {
     // This is a configuration / deployment error that should be caught before release.
     echo(
           '<pre>'
         .   "Sorry, something went wrong on the server."
-        . "\nPlease contact a yoda-portal administrator."
+        . "\nPlease contact a Yoda administrator."
         . '</pre>'
     );
     error_log(
-          'Configuration error in yoda-portal\'s public/index.php: '
+          'Configuration error in the external user service\'s public/index.php: '
         . 'Could not find App directory at \'' . $APP_ROOT . '\'.'
     );
     http_response_code(500);
