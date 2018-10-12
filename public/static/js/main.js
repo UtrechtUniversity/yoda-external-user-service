4df941448a7ea276a22e95f57354a763412e740b
Lazlo Westerhof <l.r.westerhof@uu.nl>
2018-10-12 13:09:37 +0200

    Add capitals check for username.

---
+++ public/static/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // Warn user if capitals are used in username.
+    $('#f-forgot-password-username').on('input',function(e){
+        if($('#f-forgot-password-username').val().replace(/[^A-Z]/g, "").length > 0) {
+            $('#capitals').removeClass("hidden");
+        } else {
+            $('#capitals').addClass("hidden");
+        }
+    });
+});
