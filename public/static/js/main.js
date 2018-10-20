f69d0f393304dba2e978f4c61d2219890c16af5a
Harm de Raaff <harm.apple@gmail.com>
2018-10-20 10:48:20 +0200

    User friendlier password change/reset

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,3 +1,6 @@
+// Pattern for password validation
+var patt = /^(?=.*?[A-Z])(?=.*?[0-9)(?=.*?[a-z])(?=.*?[!@#=?<>()\/\&]).{10,32}$/;
+
 $(document).ready(function() {
     // Warn user if capitals are used in username.
     $('#f-forgot-password-username').on('input',function(e){
@@ -8,40 +11,109 @@ $(document).ready(function() {
         }
     });
 
+    // solely disable submit button intially for password validation
+    $('form[validationProcess="YodaPasswordValidation"] input[type="submit"]').prop('disabled', true);
+
     $('form').submit(function(event) {
         // handles password/password again validation in forms for
         // 1) activation
         // 2) password reset
         if ($(this).attr('validationProcess')=='YodaPasswordValidation') {
-            var patt = /^(?=.*?[A-Z])(?=.*?[0-9)(?=.*?[a-z])(?=.*?[!@#=?<>()\/\&]).{10,32}$/;
-
             password        =  $('input[name="password"]').val();
             passwordAgain   =  $('input[name="password_again"]').val();
 
             if (password.length==0) {
-                $('.alert-warning').removeClass('hide').html('Please fill in a password');
+                passwordAlert('Please fill in a password', false);
             }
             else if (password != passwordAgain) {
-                $('.alert-warning').removeClass('hide').html('The entered passwords are not the same.');
+                passwordAlert('The entered passwords are not the same.', false);
             }
             else if(!patt.test(password)){
-                $('.alert-warning').removeClass('hide').html('The passwords given do not meet the requirements as stated below.');
+                passwordAlert('The passwords given do not meet the requirements as stated below.', false);
             }
             else {
-                $(this).find("input[type='submit']").prop('disabled', true);
+                $("input[type='submit']").prop('disabled', true);
                 return;
             }
             // prevent submission
             event.preventDefault();
         } else {
-            $(this).find("input[type='submit']").prop('disabled', true);
+            $("input[type='submit']").prop('disabled', true);
         }
     });
 
-// Activation handling specifics
-    $('#f-activation-submit').prop('disabled', true);
-
-    $('#cb-activation-tou').click(function(){
-        $('#f-activation-submit').prop('disabled',!$(this).prop('checked'));
+    $('#f-activation-submit').click(function(){
+        if (!$('#cb-activation-tou').prop('checked')) {
+           alertBox('Please accept the terms of use.');
+           return false;
+        }
     });
+
+    // password checking specific
+    $('input[name="password"]').keyup(checkPassword);
+    $('input[name="password_again"]').keyup(checkPasswordMatch);
+
 });
+
+// live validation
+function checkPassword()
+{
+    password        =  $('input[name="password"]').val();
+    passwordAgain   =  $('input[name="password_again"]').val();
+
+    if (password.length==0) {
+        passwordAlert('Please fill in a password', false);
+    }
+    else if(!patt.test(password)){
+        passwordAlert('Password given does not meet the requirements as stated below.', false);
+    }
+    else {
+        if (passwordAgain.length==0) {
+            passwordAlert('Please confirm your password', false);
+        }
+        else {
+            if (passwordAgain!=password) {
+                passwordAlert('Confirmation password is not equal', false);
+            }
+            else {
+                passwordAlert('Passwords are equal and conform requirements', true);
+            }
+        }
+    }
+}
+
+// hier alleen checken of ze overeenkomen?? of voor edits van beide, allebei de velden testen?
+function checkPasswordMatch()
+{
+    password        =  $('input[name="password"]').val();
+    passwordAgain   =  $('input[name="password_again"]').val();
+
+    if (password.length==0) {
+        passwordAlert('Please fill in a password', false);
+    }
+    else if (passwordAgain.length==0) {
+       passwordAlert('Please confirm your password', false);
+    }
+    else if (password != passwordAgain) {
+        passwordAlert('The confirmation passwords is not the same.', false);
+    }
+    else {
+        if(!patt.test(password)) {
+            passwordAlert('Password is not conform requirements', false);
+        }
+        else {
+            passwordAlert('Passwords are equal and conform requirements', true);
+        }
+    }
+}
+
+function passwordAlert(alert, submitState) {
+    alertBox(alert);
+    $("input[type='submit']").prop('disabled', !submitState);
+}
+
+function alertBox(alert) {
+    $('.alert-warning').fadeOut(30, function () {
+        $(this).html(alert);
+    }).fadeIn(30);
+}
